37fc587ab2b69fadb9a8d8f1bed134b11b3f3b37
zju-hh <88128734+zju-hh@users.noreply.github.com>
2023-06-08 21:10:01 +0800

    第三轮迭代前端

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,20 @@ import LoginPage from "./components/Login/LoginPage";
 import Register from "./components/Register/Register";
 import agent from "./agent";
 import ChangePassword from "./components/Register/ChangePassword";
+import ReleaseSale from "./components/Release/ReleaseSale";
+import ChangeSale from "./components/Release/ChangeSale";
+import ReleaseWanted from "./components/Release/ReleaseWanted";
+import ChangeWanted from "./components/Release/ChangeWanted";
 import Details from "./components/Details/Details";
 
 //withAuth的组件实例化
 const AuthProfile = withAuth(Profile)
 const AuthPersonItem = withAuth(PersonItem)
 const AuthChangePW = withAuth(ChangePassword)
+const AuthReleaseSale = withAuth(ReleaseSale)
+const AuthChangeSale = withAuth(ChangeSale)
+const AuthReleaseWanted = withAuth(ReleaseWanted)
+const AuthChangeWanted = withAuth(ChangeWanted)
 
 //检查是否登录
 function checkAuth() {
@@ -44,11 +52,15 @@ function App() {
             <Route exact path="/Home"  element={<Home/>} />
 			<Route path="/Login" element={<LoginPage/>} />
 			<Route path="/Register" element={<Register/>} />
-			<Route path="/Details" element={<Details/>} />
+            <Route path="/Details" element={<Details/>} />
 			{/* 需要身份验证的部分 */}
 			<Route path="/Profile"  element={<AuthProfile />} />
-            <Route path="/PersonItem"  element={<AuthPersonItem/>} />
+            		<Route path="/PersonItem"  element={<AuthPersonItem/>} />
 			<Route path="/ChangePassword" element = {<AuthChangePW/>} />
+			<Route path="/ReleaseWanted" element={<AuthReleaseWanted />} />
+			<Route path="/ChangeSale" element={<AuthChangeSale />} />
+			<Route path="/ChangeWanted" element={<AuthChangeWanted />} />
+			<Route path="/ReleaseSale" element = {<AuthReleaseSale/>}/>
         </Routes>
       </Router>
   );
